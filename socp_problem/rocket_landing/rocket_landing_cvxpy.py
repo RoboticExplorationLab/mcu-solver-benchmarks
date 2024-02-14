947cb175b2af264e30b41c2ea586faf153d92542
xkhainguyen <nxk120199@gmail.com>
2024-02-14 16:58:53 -0500

    fixed stuff, ran fine with short horizon

--- socp_problem/rocket_landing/rocket_landing_cvxpy.py
+++ socp_problem/rocket_landing/rocket_landing_cvxpy.py
@@ -24,13 +24,13 @@ def mpc_cvxpy(params, X, U, A, B, f):
     NN = Nh*nx + (Nh-1)*nu  # number of decision variables
     x0 = X[0].copy()  # initial state
 
-    z = cp.Variable(NN, nonneg=True)
+    z = cp.Variable(NN)
     inds = np.reshape(np.arange(0, NN + nu), (nx + nu, Nh), order='F')
     print('inds', inds.shape)
     xinds = [inds[:nx, i] for i in range(Nh)]
-    print('xinds', (xinds))
+    # print('xinds', (xinds))
     uinds = [inds[nx:, i] for i in range(Nh - 1)]
-    print('uinds', (uinds))
+    # print('uinds', (uinds))
 
     # Objective function
     P = np.zeros((NN, NN))
@@ -47,24 +47,25 @@ def mpc_cvxpy(params, X, U, A, B, f):
     Qf, dQf = term_cost_expansion(params)
     P[-nx:, -nx:] = Qf  
     q[-nx:] = dQf.reshape(-1, 1)    
-    print('P', P)
-    print('q', q)
+    print('P', P[0:10, 0:10])
+    print('q', q[0:10])
     if q.shape != z.shape:
         q = q.reshape(z.shape)
-    objective = cp.Minimize(0.5 * cp.quad_form(z, P) + cp.sum(cp.multiply(q, z)))
-    # objective = cp.Minimize(0.5 * cp.quad_form(z, P) + q.T @ z)
+
+    objective = cp.Minimize(0.5 * cp.quad_form(z, P) + q.T @ z)
     constraints = []
+
     # Dynamics Constraints
     for k in range(Nh - 2):
         constraints.append(A @ z[xinds[k]] + B @ z[uinds[k]] + f == z[xinds[k+1]])
+
     constraints.append(z[xinds[0]] == x0)
-    # print("z0 === ", xinds[1])
-    # constraints = []
+
     # Thrust angle constraint (SOC): norm([u1,u2]) <= alpha_max * u3
-    # if params["ncu_cone"] > 0:
-    #     for k in range(Nh-1):
-    #         u1, u2, u3 = z[uinds[k]]
-    #         constraints.append(cp.norm(cp.vstack([alpha_max * u3, u1, u2])) <= alpha_max * u3)
+    if params["ncu_cone"] > 0:
+        for k in range(Nh-1):
+            u1, u0 = z[uinds[k]][0:2], z[uinds[k]][2]
+            constraints.append(cp.norm(u1) <= 0.4 * u0)
     """
     if params['ncu_cone'] > 0:
         for k in range(Nh - 1):
@@ -82,19 +83,20 @@ def mpc_cvxpy(params, X, U, A, B, f):
     #         constraints.append(z[xinds[k]] <= params['x_max'])
     #         constraints.append(z[xinds[k]] >= params['x_min'])
 
-    # # Input constraints
-    # if params['ncu'] > 0:
-    #     for k in range(Nh-1):
-    #         constraints.append(z[uinds[k]] <= params['u_max'])
-    #         constraints.append(z[uinds[k]] >= params['u_min'])
+    # Input constraints
+    if params['ncu'] > 0:
+        for k in range(Nh-1):
+            constraints.append(z[uinds[k]] <= params['u_max'])
+            constraints.append(z[uinds[k]] >= params['u_min'])
     
-    # # Goal constraint
+    # Goal constraint
     # if params['ncg'] > 0:
-    #     constraints.append(z[xinds[N-2]] == np.zeros(nx))
+    #     constraints.append(z[xinds[N-1]] == np.zeros(nx))
     
     # Solve
     problem = cp.Problem(objective,constraints)
-    # problem.solve(verbose=True, solver = 'ECOS', feastol = 1e-4,abstol=1e-4, reltol =1e-4)
+    #problem.solve(verbose=True, feastol = 1e-4,abstol=1e-4, reltol =1e-4)
+    problem.solve(verbose=True, solver="ECOS", abstol=1e-2)
     """
     # Extract results
     for j in range(Nh-2):
@@ -102,6 +104,7 @@ def mpc_cvxpy(params, X, U, A, B, f):
         U[j] = z[k*nu : (k+1)*nu].value
     X[Nh] = z[-nx:].value
     """
+    print('z', z.value) 
 
     return U[0]
 
@@ -119,11 +122,11 @@ if __name__ == "__main__":
                   [0.005, 0.0, 0.0],
                   [0.0, 0.005, 0.0],
                   [0.0, 0.0, 0.005]])
-    f = np.array([0.0, 0.0, -0.0122625, 0.0, 0.0, -0.4905])
+    f = np.array([0.0, 0.0, -0.0122625, 0.0, 0.0, -0.4905])*0
 
     nx = 6
     nu = 3
-    N = 301
+    N = 10 # horizon length, short for MPC
     dt = 0.05
     t_vec = dt * np.arange(N)
     print('t_vec', t_vec.shape)
@@ -141,37 +144,28 @@ if __name__ == "__main__":
     # print('Uref', len(Uref))
     Q = 1e2 * np.eye(nx)
     R = 1e0 * np.eye(nu)
-    Qf = 1e3 * np.eye(nx)
+    Qf = 1e2 * np.eye(nx)
 
     gravity = np.array([0, 0, -9.81])
     mass = 10.0
     perWeightMax = 2.0
     θ_thrust_max = 5.0  # deg
 
-    A_cone = np.array([[1, 0, 0], [0, 1, 0]])
+    A_cone = np.array([[1, 0, 0], [0, 1.0, 0]])
     c_cone = np.array([0.0, 0.0, np.tan(np.radians(θ_thrust_max))])
     u_bnd = mass * np.abs(gravity[2]) * perWeightMax
-    print('u_bnd', u_bnd.shape)
+    # print('u_bnd', u_bnd.shape)
     # Sloppy bounds to test
-    u_min = -10.0 * np.ones(nu)
+    u_min = 20 * np.ones(nu)
     u_max = 105.0 * np.ones(nu)
     x_min = [-5, -5, 0, -10, -10, -10.0]
     x_max = [5, 5, 20, 10, 10, 10.0]
 
     ncx = 0
-    ncu = 2 * nu*1
+    ncu = 1
     ncg = 0
-    ncu_cone = 0
+    ncu_cone = 1
     cone_scale = 1e-3
-
-    μ = [np.zeros(nu) for _ in range(N - 1)]  # input constraints
-    print('μ', len(μ))
-    μx = [np.zeros(nx) for _ in range(N)]  # state constraints
-    print('μx', len(μx))
-    λ = np.zeros(nx)  # goal constraint
-    print('λ', len(λ))
-    λc = [np.zeros(ncu_cone) for _ in range(N - 1)]  # goal constraint
-    print('λc', len(λc))
 
     params = {
         'nx': nx,
@@ -180,10 +174,6 @@ if __name__ == "__main__":
         'ncu': ncu,
         'ncg': ncg,
         'ncu_cone': ncu_cone,
-        'μ': μ,
-        'μx': μx,
-        'λ': λ,
-        'λc': λc,
         'A_cone': A_cone,
         'c_cone': c_cone,
         'N': N,
