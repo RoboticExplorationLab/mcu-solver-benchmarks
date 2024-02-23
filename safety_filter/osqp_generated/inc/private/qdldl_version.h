8cf02b4f56a003746f835f08f5d9cec3cdfe7976
Sam Schoedel <seschoedel@gmail.com>
2024-02-23 12:04:22 -0500

    Delete osqp generated files

--- safety_filter/osqp_generated/inc/private/qdldl_version.h
+++
@@ -1,8 +0,0 @@
-#ifndef QDLDL_VERSION_H_
-#define QDLDL_VERSION_H_
-
-#define QDLDL_VERSION_MAJOR     0
-#define QDLDL_VERSION_MINOR     1
-#define QDLDL_VERSION_PATCH     6
-
-#endif // #ifndef QDLDL_VERSION_H_
